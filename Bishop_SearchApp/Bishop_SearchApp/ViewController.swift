d60fff0d45b8e635db7d5e0132e90d4a0d61d95a
Mackenzie Bishop <69914681+mackenziebishop@users.noreply.github.com>
2022-10-13 10:41:33 -0500

    Finished UI Design

---
+++ Bishop_SearchApp/Bishop_SearchApp/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Bishop_SearchApp
+//
+//  Created by Bishop,Mackenzie N on 10/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    @IBOutlet weak var searchButtonAction: UIButton!
+    
+    @IBOutlet weak var resultImage: UIImageView!
+    
+    @IBOutlet weak var showNextImagesBtn: UIButton!
+    
+    @IBOutlet weak var showPrevImagesBtn: UIButton!
+    
+    @IBOutlet weak var topicInfoText: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Bishop_SearchApp/Bishop_SearchAppUITests/Bishop_SearchAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Bishop_SearchAppUITestsLaunchTests.swift
+//  Bishop_SearchAppUITests
+//
+//  Created by Bishop,Mackenzie N on 10/11/22.
+//
+
+import XCTest
+
+class Bishop_SearchAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
